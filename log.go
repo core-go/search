cb529d703d2a5a76cfb828f4828bac996ee776c1
Duc Nguyen <minhduc140583@gmail.com>
2021-10-10 17:18:28 +0700

    Refactor respond JSON

--- log.go
+++ log.go
@@ -13,10 +13,12 @@ func respondError(w http.ResponseWriter, r *http.Request, code int, result inter
 	respond(w, r, code, result, writeLog, resource, action, false, err.Error())
 }
 func respond(w http.ResponseWriter, r *http.Request, code int, result interface{}, writeLog func(ctx context.Context, resource string, action string, success bool, desc string) error, resource string, action string, success bool, desc string) {
-	response, _ := json.Marshal(result)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(code)
-	w.Write(response)
+	err := json.NewEncoder(w).Encode(result)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 	if writeLog != nil {
 		writeLog(r.Context(), resource, action, success, desc)
 	}
